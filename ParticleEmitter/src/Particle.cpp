72a2b14e31cc3eead1b376cd97e804aad90b30b4
cnpatton <botanicality@gmail.com>
2018-10-05 23:33:57 -0500

    Added the repositioning of particles off screen and cleaned up particle class

--- ParticleEmitter/src/Particle.cpp
+++ ParticleEmitter/src/Particle.cpp
@@ -8,17 +8,33 @@ using namespace std;
 
 Particle::Particle():m_x(0), m_y(0)
 {
+    init();
+}
+
+void Particle::init(){
     // Angle from 0 -> 2PI
+    m_x = 0;
+    m_y = 0;
     m_direction = (2 * M_PIl * rand())/RAND_MAX;
-    m_speed = (0.0001 * rand())/RAND_MAX;
+    m_speed = (0.04 * rand())/RAND_MAX;
+    // square it to achieve an exaggerated distribution as you move towards the edge
+    m_speed *= m_speed;
 }
 
 void Particle::update(int interval){
+    m_direction += interval * 0.0003;
+
     double xspeed = m_speed * cos(m_direction);
     double yspeed = m_speed * sin(m_direction);
 
     m_x += xspeed * interval;
     m_y += yspeed * interval;
+
+    // reset particles that go off the screen to get new direction and speed from the center of the screen
+    if(m_x < -1 || m_x > 1 || m_y < -1 || m_y > 1){
+        init();
+    }
+
 }
 
 Particle::~Particle()
